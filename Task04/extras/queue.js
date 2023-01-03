879898febeb3563a5cee08a4b56fdce08dbd22b2
illia-git <ichykurov@gmail.com>
2023-01-03 14:28:10 +0100

    fix Queue class

--- Task04/extras/queue.js
+++ Task04/extras/queue.js
@@ -1,21 +1,50 @@
+class Node {
+    constructor(value, next = null) {
+        this.value = value;
+        this.next = next;
+    }
+}
+
 class Queue {
     constructor() {
-        this.items = [];
+        this.first = null;
+        this.last = null;
+        this.length = 0;
     }
 
-    enqueue(element) {
-        this.items.push(element);
+    enqueue(value) {
+        const newNode = new Node(value);
+        if (this.isEmpty()) {
+            this.first = newNode;
+            this.last = newNode;
+        } else {
+            this.last.next = newNode;
+            this.last = newNode;
+        }
+        this.length += 1;
     }
 
     dequeue() {
-        return this.items.shift();
+        if (this.isEmpty()) {
+            return null;
+        }
+        const value = this.first.value;
+        this.first = this.first.next;
+        this.length -= 1;
+        if (this.isEmpty()) {
+            this.last = null;
+        }
+        return value;
     }
 
     peek() {
-        return this.items[0];
+        if (this.isEmpty()) {
+            return null;
+        }
+        return this.first.value;
     }
 
     isEmpty() {
-        return this.items.length === 0;
+        return this.length === 0;
     }
 }
